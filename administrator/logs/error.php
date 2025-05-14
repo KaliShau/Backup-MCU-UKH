297584472f4d78c99d86d0bdfea33c417e770ecc
KaliShau <fa6248122add@gmail.com>
2025-05-14 13:57:14 +0300

    docs: update README.md

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2025-05-14 08:52:59 UTC
+#Software: Joomla! 5.3.0 Stable [ Timu ] 15-April-2025 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2025-05-14T08:52:59+00:00	INFO 95.153.176.108	joomlafailure	Username and password do not match or you do not have an account yet.
